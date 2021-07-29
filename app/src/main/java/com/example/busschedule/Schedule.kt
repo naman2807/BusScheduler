ca287db00eb73715891b8d794ce765d838ae0e38
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-29 12:11:54 +0530

    Add a column for the name of the bus stop. The column should be of type String.

--- app/src/main/java/com/example/busschedule/Schedule.kt
+++ app/src/main/java/com/example/busschedule/Schedule.kt
@@ -1,7 +1,10 @@
 package com.example.busschedule
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
 
 data class Schedule(
-    @PrimaryKey val id: Int
+    @PrimaryKey val id: Int,
+    @NonNull @ColumnInfo(name = "stop_name") val stopName: String
 )
