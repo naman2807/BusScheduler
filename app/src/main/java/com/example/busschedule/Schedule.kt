946b4e6f62648997bc5505f083a12485911502cb
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-29 12:13:19 +0530

    Added the arrival time column

--- app/src/main/java/com/example/busschedule/Schedule.kt
+++ app/src/main/java/com/example/busschedule/Schedule.kt
@@ -6,5 +6,6 @@ import androidx.room.PrimaryKey
 
 data class Schedule(
     @PrimaryKey val id: Int,
-    @NonNull @ColumnInfo(name = "stop_name") val stopName: String
+    @NonNull @ColumnInfo(name = "stop_name") val stopName: String,
+    @NonNull @ColumnInfo(name = "arrival_time") val arrivalTime: Int
 )
