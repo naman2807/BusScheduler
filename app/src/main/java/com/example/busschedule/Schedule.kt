3a58e77b6171a4e4e69dcbd1a9ea50f98fe1078f
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-29 12:13:57 +0530

    Add @Entity on a separate line before the class name.

--- app/src/main/java/com/example/busschedule/Schedule.kt
+++ app/src/main/java/com/example/busschedule/Schedule.kt
@@ -2,8 +2,10 @@ package com.example.busschedule
 
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class Schedule(
     @PrimaryKey val id: Int,
     @NonNull @ColumnInfo(name = "stop_name") val stopName: String,
