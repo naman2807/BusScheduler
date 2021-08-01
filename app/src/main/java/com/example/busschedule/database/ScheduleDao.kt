fd8627bce278ec4de929774a686565d91a3b06f5
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:10:59 +0530

    Define a getByStopName() function that takes a String parameter called stopName and returns a List of Schedule objects, with a @Query annotation

--- app/src/main/java/com/example/busschedule/database/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/ScheduleDao.kt
@@ -7,4 +7,7 @@ import androidx.room.Query
 interface ScheduleDao {
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
     fun getAll(): List<Schedule>
+
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
 }
