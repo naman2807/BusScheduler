bcf0d41418c4898f4f62da4ae14bad4d4d2e2ece
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 17:05:23 +0530

    Implemented the bind() method

--- app/src/main/java/com/example/busschedule/adapter/BusStopAdapter.kt
+++ app/src/main/java/com/example/busschedule/adapter/BusStopAdapter.kt
@@ -1,12 +1,23 @@
 package com.example.busschedule.adapter
 
+import android.annotation.SuppressLint
 import android.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.database.Schedule
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit): ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallBack) {
 
-    class BusStopViewHolder {
-
+    class BusStopViewHolder(private val binding: BusStopItemBinding):RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SimpleDateFormat")
+        fun bind(schedule: Schedule) {
+            binding.stopNameTextView.text = schedule.stopName
+            binding.arrivalTimeTextView.text = SimpleDateFormat(
+                "h:mm a").format(
+                Date(schedule.arrivalTime.toLong() * 1000)
+            )
+        }
     }
 }
