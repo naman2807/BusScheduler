54b540abde627912d6ed250127a36b40dd62d495
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:25:00 +0530

    Implemented create() method

--- app/src/main/java/com/example/busschedule/viewmodel/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodel/BusScheduleViewModel.kt
@@ -21,6 +21,10 @@ class BusScheduleViewModel(private val scheduleDao: ScheduleDao):ViewModel() {
 
 class BusScheduleViewModelFactory(private val scheduleDao: ScheduleDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("Not yet implemented")
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
