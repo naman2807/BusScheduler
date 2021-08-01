0242543ad066b64da0277d5fec6bb3d6278d8ea4
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:37:14 +0530

    Add the @Database annotation

--- app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -1,12 +1,30 @@
 package com.example.busschedule.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = arrayOf(Schedule::class), version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun scheduleDao(): ScheduleDao
 
     companion object{
         @Volatile
         private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+            val instance = Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "app_database")
+                .createFromAsset("database/bus_schedule.db")
+                .build()
+            INSTANCE = instance
+
+                instance
+
+        }
     }
+  }
 }
