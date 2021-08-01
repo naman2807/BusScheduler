8e23c006e138bf7efdda236fbc9fd15f35b97522
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:20:59 +0530

    Created a scheduleForStopName() method

--- app/src/main/java/com/example/busschedule/viewmodel/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodel/BusScheduleViewModel.kt
@@ -6,4 +6,5 @@ import com.example.busschedule.database.ScheduleDao
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao):ViewModel() {
     fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
 }
