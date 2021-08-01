dba397e4f1eff047e878355e443f009108d7e54b
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 17:08:59 +0530

    Overridden the methods

--- app/src/main/java/com/example/busschedule/adapter/BusStopAdapter.kt
+++ app/src/main/java/com/example/busschedule/adapter/BusStopAdapter.kt
@@ -1,16 +1,17 @@
 package com.example.busschedule.adapter
 
 import android.annotation.SuppressLint
-import android.widget.ListAdapter
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.database.Schedule
 import com.example.busschedule.databinding.BusStopItemBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
-class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit): ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallBack) {
+class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallBack) {
 
-    class BusStopViewHolder(private val binding: BusStopItemBinding):RecyclerView.ViewHolder(binding.root) {
+    class BusStopViewHolder(private val binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SimpleDateFormat")
         fun bind(schedule: Schedule) {
             binding.stopNameTextView.text = schedule.stopName
@@ -19,5 +20,13 @@ class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit): ListAdapter
                 Date(schedule.arrivalTime.toLong() * 1000)
             )
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BusStopViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BusStopViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 }
