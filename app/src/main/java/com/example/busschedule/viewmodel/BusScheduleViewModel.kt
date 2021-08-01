ac9d3762d540dddba9b33b978d483e08830bfeb0
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:20:30 +0530

    Created a fullSchedule() method

--- app/src/main/java/com/example/busschedule/viewmodel/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodel/BusScheduleViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.busschedule.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.busschedule.database.Schedule
 import com.example.busschedule.database.ScheduleDao
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao):ViewModel() {
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 }
