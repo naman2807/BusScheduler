10ef9e5846feb0495c973e913af6ac84edb63946
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:09:20 +0530

    Define a function getAll() that returns a List of Schedule objects including the @Query annotation

--- app/src/main/java/com/example/busschedule/database/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/ScheduleDao.kt
@@ -1,8 +1,10 @@
 package com.example.busschedule.database
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 interface ScheduleDao {
-
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
 }
