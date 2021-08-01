44bfa7e398d0351f754b0519693aeb748139a2f5
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-01 12:41:17 +0530

    Add a database property of type AppDatabase. The property should be lazy and return the result of calling getDatabase() on your AppDatabase class.

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -1,6 +1,10 @@
 package com.example.busschedule
 
 import android.app.Application
+import com.example.busschedule.database.AppDatabase
 
 class BusScheduleApplication: Application() {
+    val database : AppDatabase by lazy {
+        AppDatabase.getDatabase(context = this)
+    }
 }
